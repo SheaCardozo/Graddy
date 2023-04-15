25450d75bcf12a14f9fca85aa24fc14eaa6ac48d
SheaCardozo <sacardoz@uwaterloo.ca>
2023-04-15 15:30:22 -0500

    find

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='graddy',
       version='0.0.1',
       description='Graddy ML Framework',
       author='Shea Cardozo',
       author_email='sacardoz@uwaterloo.ca',
-      packages=['graddy'],
+      packages=find_packages(),
       install_requires=[],
       zip_safe=False)
