8d7984752753fcf49e9949670067b1034019f8c5
SheaCardozo <sacardoz@uwaterloo.ca>
2023-04-15 14:54:04 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Graddy',
+      version='0.0.1',
+      description='Graddy ML Framework',
+      author='Shea Cardozo',
+      author_email='sacardoz@uwaterloo.ca',
+      packages=['graddy'],
+      install_requires=[],
+      zip_safe=False)
