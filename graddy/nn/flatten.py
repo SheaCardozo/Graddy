3a01d17bb5ce7f31e738760c3f2c57ed93f0abc4
SheaCardozo <sacardoz@uwaterloo.ca>
2023-04-17 03:32:49 -0500

    small flatten fix

--- graddy/nn/flatten.py
+++ graddy/nn/flatten.py
@@ -2,20 +2,21 @@ from graddy.nn.module import Module
 from graddy.engine.tensor import Tensor
 
 class Flatten(Module):
-  def __init__(self):
+  def __init__(self, start_dim=0):
     super().__init__()
+    self.start_dim = start_dim
 
-  def _flatten(self, x):
+  def _flatten(self, x, count):
 
     if not isinstance(x, Tensor):
       return x
     else:
-      if len(x) == 1:
-        return self._flatten(x[0])
+      if len(x) == 1 and count >= self.start_dim:
+        return self._flatten(x[0], count+1)
       else:
-        return Tensor([self._flatten(xt) for xt in x])
+        return Tensor([self._flatten(xt, count+1) for xt in x])
 
 
   def __call__(self, x):
-    x = self._flatten(x)
+    x = self._flatten(x, count=0)
     return x
